30dcfcc4a681c77619ed9d658ce7156532d90cc1
yves songolo <yves.songolo@gmail.com>
2018-05-14 17:14:00 -0700

    Initial Commit

---
+++ appleMovie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appleMovie
+//
+//  Created by Yveslym on 5/14/18.
+//  Copyright © 2018 yveslym. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
